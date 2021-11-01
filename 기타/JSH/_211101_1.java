38d3b0166d097061ac57f517bf27f8696a085865
2giduck <cosmos7256@naver.com>
2021-11-01 22:39:32 +0900

    211101

---
+++ 기타/JSH/_211101_1.java
@@ -0,0 +1,12 @@
+package study_of_hell.기타.JSH;
+
+public class _211101_1 {
+    public static void main(String[] args) {
+        System.err.println(new _211101_1().solution(28));
+        System.err.println(new _211101_1().solution(734));
+        System.err.println(new _211101_1().solution(1990));
+        System.err.println(new _211101_1().solution(1000));
+    }
+
+
+}
