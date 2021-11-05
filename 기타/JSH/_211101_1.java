eef1ad98f1eead03a347c9604d2acf77200bfbff
2giduck <cosmos7256@naver.com>
2021-11-05 12:03:32 +0900

    211105

--- 기타/JSH/_211101_1.java
+++ 기타/JSH/_211101_1.java
@@ -2,11 +2,32 @@ package study_of_hell.기타.JSH;
 
 public class _211101_1 {
     public static void main(String[] args) {
-//        System.err.println(new _211101_1().solution(28));
-//        System.err.println(new _211101_1().solution(734));
-//        System.err.println(new _211101_1().solution(1990));
-//        System.err.println(new _211101_1().solution(1000));
+        System.err.println(new _211101_1().solution(28));
+        System.err.println(new _211101_1().solution(734));
+        System.err.println(new _211101_1().solution(1990));
+        System.err.println(new _211101_1().solution(1000));
     }
 
+    public int solution(int N) {
+        // write your code in Java SE 8
+        int sumOfDigit = getSumOfDigit(N);
 
+        int nextNum = N+1;
+        int sum = getSumOfDigit(nextNum);
+
+        while(sum!=sumOfDigit){
+            sum = getSumOfDigit(++nextNum);
+        }
+
+        return nextNum;
+    }
+
+    public int getSumOfDigit(int N){
+        int sum =0;
+        while(N!=0){
+            sum += N%10;
+            N/=10;
+        }
+        return sum;
+    }
 }
