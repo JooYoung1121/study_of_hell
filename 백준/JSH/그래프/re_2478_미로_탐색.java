4f2b1da04629a4630182534bc82308f009e716fc
2giduck <cosmos7256@naver.com>
2021-11-24 10:28:57 +0900

    211124

---
+++ 백준/JSH/그래프/re_2478_미로_탐색.java
@@ -0,0 +1,22 @@
+package study_of_hell.백준.JSH.그래프;
+
+import java.util.Scanner;
+
+public class re_2478_미로_탐색 {
+    static Scanner sc = new Scanner(System.in);
+    static StringBuilder sb = new StringBuilder();
+
+
+    static void input(){
+
+    }
+
+    static void pro(){
+
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+}
