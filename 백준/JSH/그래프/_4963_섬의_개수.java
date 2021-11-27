f9e888636b23bb5841ec1a0fd1333d9b745a238f
2giduck <cosmos7256@naver.com>
2021-11-28 01:13:56 +0900

    211128

--- 백준/JSH/그래프/_4963_섬의_개수.java
+++ 백준/JSH/그래프/_4963_섬의_개수.java
@@ -3,74 +3,63 @@ package study_of_hell.백준.JSH.그래프;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class _1012_유기농_배추 {
+public class _4963_섬의_개수 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int M; // 배추밭의 가로길이
-    static int N; // 배추밭의 세로길이
-    static int K; // 배추가 심어져 있는 위치의 갯수
-    static int[][] a;
-    static int[][] cabbage;
+    static int w;   // 지도의 너비
+    static int h;   // 지도의 높이
+    static int[][] map;
     static boolean[][] visit;
+    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
     static int count;
-    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
     static void input(){
-        M=scan.nextInt();
-        N=scan.nextInt();
-        K=scan.nextInt();
-        a = new int[M][N];
-        cabbage = new int[K][2];
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                a[i][j] =0;
+        map = new int[w][h];
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                map[i][j]=scan.nextInt();
             }
         }
-        for (int i = 0; i < K; i++) {
-            int x = scan.nextInt();
-            int y = scan.nextInt();
-            cabbage[i][0]=x;
-            cabbage[i][1]=y;
-            a[x][y] = 1;
-        }
-        visit = new boolean[M][N];
+        visit = new boolean[w][h];
     }
 
     static void dfs(int x, int y){
-        visit[x][y]=true;
+        visit[x][y]= true;
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x+ dir[i][0];
-            int ny = y+dir[i][1];
-
-            if(nx<0 || ny<0 || nx>=M || ny>=N) continue;
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dir[i][0];
+            int ny = y + dir[i][1];
+            if(nx<0 || ny<0 || nx>=w || ny>=h) continue;
             if(visit[nx][ny]) continue;
-            if(a[nx][ny]==0) continue;
+            if(map[nx][ny]==0) continue;
             dfs(nx, ny);
         }
+
     }
 
     static void pro(){
         count=0;
-        for (int i = 0; i < K; i++) {
-            int x = cabbage[i][0];
-            int y = cabbage[i][1];
-            if(!visit[x][y]&&a[x][y]==1){
-                count++;
-                dfs(x, y);
+
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                if(!visit[i][j]&&map[i][j]==1) {
+                    count++;
+                    dfs(i, j);
+                }
             }
         }
-        sb.append(count + "\n");
+        sb.append(count+"\n");
     }
 
     public static void main(String[] args) {
-        int testCount = scan.nextInt();
-        for (int i = 0; i < testCount; i++) {
+        while(true){
+            w = scan.nextInt();
+            h = scan.nextInt();
+            if(w==0 && h==0) break;
             input();
             pro();
         }
-
         System.out.println(sb.toString());
     }
 
