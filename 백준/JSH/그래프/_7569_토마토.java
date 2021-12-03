7ccb100357206b05195b282a2f7cf58918429947
2giduck <cosmos7256@naver.com>
2021-12-03 09:31:07 +0900

    211203

---
+++ 백준/JSH/그래프/_7569_토마토.java
@@ -0,0 +1,68 @@
+package study_of_hell.백준.JSH.그래프;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _7569_토마토 {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static void input(){
+
+    }
+
+    static void pro(){
+
+    }
+
+    public static void main(String[] args) {
+        input();
+
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
