44cf2304f4a9c1a85f01e414c4024ab0c7bf9b46
2giduck <cosmos7256@naver.com>
2021-12-13 18:42:10 +0900

    211213

--- 백준/JSH/완전탐색/re_14888_연산자_끼워넣기.java
+++ 백준/JSH/완전탐색/re_14888_연산자_끼워넣기.java
@@ -1,53 +1,25 @@
-package study_of_hell.백준.JSH.백트래킹;
+package study_of_hell.백준.JSH.완전탐색;
 
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class _15650_N과_M_2 {
-    static StringBuilder sb = new StringBuilder();
-
-    static void input(){
-        FastReader scan = new FastReader();
-        N= scan.nextInt();
-        M= scan.nextInt();
-        selected = new int[M+1];
-    }
-
-    static int N, M;
-    static int[] selected, used;
-
-    static void rec_func(int k){
-        if(k==M+1){
-            for(int i=1;i<=M;i++) sb.append(selected[i]).append(' ');
-            sb.append('\n');
-        }else{
-            for(int cand = selected[k-1] +1; cand<=N;cand++){
-                selected[k] = cand;
-                // k+1 번 ~ M번을 모두 탐색하는 일을 해야 함
-                rec_func(k+1);
-                selected[k]=0;
-            }
-
-        }
-    }
-
+public class re_14888_연산자_끼워넣기 {
     public static void main(String[] args) {
-        input();
-
-        rec_func(1);
-        System.out.println(sb.toString());
     }
 
 
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
+
         public FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
+
         public FastReader(String s) throws FileNotFoundException {
             br = new BufferedReader(new FileReader(new File(s)));
         }
+
         String next() {
             while (st == null || !st.hasMoreElements()) {
                 try {
@@ -58,15 +30,19 @@ public class _15650_N과_M_2 {
             }
             return st.nextToken();
         }
+
         int nextInt() {
             return Integer.parseInt(next());
         }
+
         long nextLong() {
             return Long.parseLong(next());
         }
+
         double nextDouble() {
             return Double.parseDouble(next());
         }
+
         String nextLine() {
             String str = "";
             try {
--- 백준/JSH/완전탐색/re_15649_N과_M_1.java
+++ 백준/JSH/완전탐색/re_15649_N과_M_1.java
@@ -1,41 +1,17 @@
-package study_of_hell.백준.JSH.백트래킹;
+package study_of_hell.백준.JSH.완전탐색;
 
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class _15651_N과_M_3 {
+public class re_15649_N과_M_1 {
     static StringBuilder sb = new StringBuilder();
-
+    static FastReader scan = new FastReader();
     static void input(){
-        FastReader scan = new FastReader();
-        N= scan.nextInt();
-        M= scan.nextInt();
-        selected = new int[M+1];
     }
 
-    static int N, M;
-    static int[] selected;
-
-    static void rec_func(int k){
-        if(k==M+1){
-            for(int i=1;i<=M;i++) sb.append(selected[i]).append(' ');
-            sb.append('\n');
-        }else{
-            for(int cand = 1; cand<=N;cand++){
-                selected[k] = cand;
-                // k+1 번 ~ M번을 모두 탐색하는 일을 해야 함
-                rec_func(k+1);
-                selected[k]=0;
-            }
-
-        }
-    }
 
     public static void main(String[] args) {
         input();
-
-        rec_func(1);
-        System.out.println(sb.toString());
     }
 
 
