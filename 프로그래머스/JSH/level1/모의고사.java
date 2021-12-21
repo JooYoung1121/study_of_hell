42e3158d69bd4a2f9cde691caa70019d56adb6fb
2giduck <cosmos7256@naver.com>
2021-12-21 10:40:37 +0900

    211221 - 완전탐색

--- 프로그래머스/JSH/level1/모의고사.java
+++ 프로그래머스/JSH/level1/모의고사.java
@@ -1,68 +1,42 @@
 package study_of_hell.프로그래머스.JSH.level1;
 
-//수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
-//
-//        1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
-//        2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
-//        3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
-//
-//        1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
-//
-//        제한 조건
-//        시험은 최대 10,000 문제로 구성되어있습니다.
-//        문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
-//        가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
-//        입출력 예
-//        answers	return
-//        [1,2,3,4,5]	[1]
-//        [1,3,2,4,2]	[1,2,3]
-//        입출력 예 설명
-//        입출력 예 #1
-//
-//        수포자 1은 모든 문제를 맞혔습니다.
-//        수포자 2는 모든 문제를 틀렸습니다.
-//        수포자 3은 모든 문제를 틀렸습니다.
-//        따라서 가장 문제를 많이 맞힌 사람은 수포자 1입니다.
-//
-//        입출력 예 #2
-//
-//        모든 사람이 2문제씩을 맞췄습니다.
-
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class 모의고사 {
     public static void main(String[] args) {
         int[] answers1 = {1, 2, 3, 4, 5};
         int[] answers2 = {1, 3, 2, 4, 2};
 
-        for(int a: solution(answers1)){
-            System.err.println(a);
-        }
-        for(int a: solution(answers2)){
-            System.err.println(a);
-        }
+        System.out.println(Arrays.toString(solution(answers1)));
+        System.out.println(Arrays.toString(solution(answers2)));
     }
 
     public static int[] solution(int[] answers) {
-        int[] answer = new int[3];
+        int[] person1 = {1,2,3,4,5};
+        int[] person2 = {2,1,2,3,2,4,2,5};
+        int[] person3 = {3,3,1,1,2,2,4,4,5,5};      // 수학 문제를 찍는 패턴
 
-        int[] person1 = {1, 2, 3, 4, 5};
-        int[] person2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-
-        for(int i = 0;i<answers.length;i++){
-            if(person1[i%person1.length]==answers[i]) answer[0]++;
-            if(person2[i%person2.length]==answers[i]) answer[1]++;
-            if(person3[i%person3.length]==answers[i]) answer[2]++;
+        int s1=0, s2=0, s3 = 0;
+        for (int i = 0; i < answers.length; i++) {
+            if(person1[i%person1.length]==answers[i]) s1++;
+            if(person2[i%person2.length]==answers[i]) s2++;
+            if(person3[i%person3.length]==answers[i]) s3++;
         }
 
-        int max = Math.max(Math.max(answer[0], answer[1]), answer[2]);
+        int max = Math.max(Math.max(s1, s1), s3);
 
-        ArrayList<Integer> list = new ArrayList<>();
-        if(max==answer[0]) list.add(1);
-        if(max==answer[1]) list.add(2);
-        if(max==answer[2]) list.add(3);
+        List<Integer> list = new ArrayList<>();
+        if(max==s1) list.add(1);
+        if(max==s2) list.add(2);
+        if(max==s3) list.add(3);
 
-        return list.stream().mapToInt(i->i.intValue()).toArray();
+        int[] answer = new int[ list.size()];
+        for( int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+
+        return answer;
     }
 }
