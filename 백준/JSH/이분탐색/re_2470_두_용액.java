c27ce57b20600df820164705d7f6608994c4bac8
2giduck <cosmos7256@naver.com>
2021-12-28 22:39:21 +0900

    211228 - 이분탐색

--- 백준/JSH/이분탐색/re_2470_두_용액.java
+++ 백준/JSH/이분탐색/re_2470_두_용액.java
@@ -1,19 +1,23 @@
 package study_of_hell.백준.JSH.이분탐색;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class re_2470_두_용액 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
+    static int n;
+    static int[] adj;
+
     static void input() {
-
+        n= scan.nextInt();
+        adj = new int[n+1];
+        for (int i = 1; i <=n ; i++) {
+            adj[i] = scan.nextInt();
+        }
     }
-
     static void pro(){
-
     }
 
     public static void main(String[] args) {
