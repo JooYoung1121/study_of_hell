570fd07c8e552e61784712845424723e5d7433e6
2giduck <cosmos7256@naver.com>
2022-01-11 19:54:14 +0900

    220111

--- 백준/JSH/Silver/tier4/re_11652_카드.java
+++ 백준/JSH/Silver/tier4/re_11652_카드.java
@@ -5,21 +5,46 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class re_11652_카드 {
+    static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static void input(){
-    }
+    static int n;
+    static long[] card;
 
+    static void input(){
+        n=scan.nextInt();
+        card = new long[n+1];
+        for (int i = 1; i <= n ; i++) {
+            card[i] = scan.nextLong();
+        }
+    }
 
     static void pro(){
-    }
+        Arrays.sort(card, 1, n+1);
 
+        int count=1;
+        int ans = 1;    // 갯수
+        long res = card[1];       // 정답
+        for (int i = 2; i <= n ; i++) {
+            if(card[i-1]==card[i]){  // 첫번째 숫자가 아니면서 이전과 같은숫자면
+                count++;
+            }else{
+                count=1;
+            }
+
+            if(count>ans){
+                res = card[i];
+                ans = count;
+            }
+        }
+
+        System.out.println(res);
+    }
 
     public static void main(String[] args) {
         input();
         pro();
     }
-
 
     static class FastReader {
         BufferedReader br;
