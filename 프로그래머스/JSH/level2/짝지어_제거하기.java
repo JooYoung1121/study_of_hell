6911274b80b4298952b2de885f58498bb0d39587
2giduck <cosmos7256@naver.com>
2022-03-02 21:56:23 +0900

    220302

---
+++ 프로그래머스/JSH/level2/짝지어_제거하기.java
@@ -0,0 +1,19 @@
+package study_of_hell.프로그래머스.JSH.level2;
+
+public class 짝지어_제거하기 {
+    public static void main(String[] args) {
+        String a = "baabaa";
+        String a1 ="cdcd";
+        System.err.println(solution(a));
+        System.err.println(solution(a1));
+    }
+
+    public static int solution(String s)
+    {
+        int answer = 1;
+
+        return answer;
+    }
+
+
+}
