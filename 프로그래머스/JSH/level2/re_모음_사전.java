047b8b1d980380f261cce6b0e912fc9e37a709c6
2giduck <cosmos7256@naver.com>
2022-03-22 22:32:50 +0900

    220322

---
+++ 프로그래머스/JSH/level2/re_모음_사전.java
@@ -0,0 +1,18 @@
+package study_of_hell.프로그래머스.JSH.level2;
+
+import java.util.Map;
+
+public class re_모음_사전 {
+    public static void main(String[] args) {
+        String w1 = "AAAAE", w2 = "AAAE", w3 = "I", w4 = "EIO";
+        System.err.println(solution(w1));
+        System.err.println(solution(w2));
+        System.err.println(solution(w3));
+        System.err.println(solution(w4));
+    }
+
+    public static int solution(String word) {
+        int answer = 0;
+        return answer;
+    }
+}
