2ac9f8b2cba010944f3fa8d0f21ccedae5fcce6f
2giduck <cosmos7256@naver.com>
2022-03-29 22:03:58 +0900

    220329

---
+++ 프로그래머스/JSH/level2/re_주식가격.java
@@ -0,0 +1,14 @@
+package study_of_hell.프로그래머스.JSH.level2;
+
+import java.util.Arrays;
+
+public class re_주식가격 {
+    public static void main(String[] args) {
+        int[] p = {1,2,3,2,3};
+        System.err.println(Arrays.toString(solution(p)));
+    }
+
+    public static int[] solution(int[] prices) {
+        return null;
+    }
+}
