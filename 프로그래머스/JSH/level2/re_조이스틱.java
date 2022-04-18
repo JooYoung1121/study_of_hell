e042ced5fac70e06de2c89f106fb946542d59a4e
2giduck <cosmos7256@naver.com>
2022-04-18 23:14:18 +0900

    220418

---
+++ 프로그래머스/JSH/level2/re_조이스틱.java
@@ -0,0 +1,14 @@
+package study_of_hell.프로그래머스.JSH.level2;
+
+import java.util.ArrayList;
+
+public class re_조이스틱 {
+    public static void main(String[] args) {
+        System.err.println(solution("JAZ"));
+    }
+
+    public static int solution(String name){
+        return 0;
+
+    }
+}
