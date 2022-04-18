446c1a021bd0f0aa1e73b10b0267bb39293b0429
2giduck <cosmos7256@naver.com>
2022-04-18 23:14:14 +0900

    220418

---
+++ 프로그래머스/JSH/level2/re_삼각_달팽이.java
@@ -0,0 +1,15 @@
+package study_of_hell.프로그래머스.JSH.level2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class re_삼각_달팽이 {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int[] solution(int n) {
+        return null;
+    }
+}
