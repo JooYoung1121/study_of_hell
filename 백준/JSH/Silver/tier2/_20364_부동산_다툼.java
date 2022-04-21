8c7374bff5e67a8c1a055cde03949c1ed4b5b51e
2giduck <cosmos7256@naver.com>
2022-04-21 22:12:23 +0900

    220421

--- 백준/JSH/Silver/tier2/_20364_부동산_다툼.java
+++ 백준/JSH/Silver/tier2/_20364_부동산_다툼.java
@@ -1,19 +1,57 @@
 package study_of_hell.백준.JSH.Silver.tier2;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class re_20364_부동산_다툼 {
+public class _20364_부동산_다툼 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
     static void input(){
+        n = scan.nextInt();
+        q = scan.nextInt();
+        qList = new int[q];
+        for (int i = 0; i < q; i++) {
+            qList[i] = scan.nextInt();
+        }
+        nList = new boolean[n+1];
+    }
+
+    static int n;
+    static int q;
+    static int[] qList;
+    static boolean[] nList;
+
+    static List<Integer> getList(int duck){
+        List<Integer> list = new ArrayList<>();
+
+        while(duck != 1){
+            list.add(duck);
+            duck = duck % 2 == 0 ? duck / 2 : (duck - 1)/2;
+        }
+
+        Collections.reverse(list);
+        return list;
     }
 
     static void pro(){
+        for (int duck: qList){
+            List<Integer> parentList = getList(duck);
 
+            boolean check = false;
+            int temp = 0;
+            for(int parent : parentList){
+                if(nList[parent]){
+                    check = true; temp = parent;
+                    break;
+                }
+            }
+            if(!check) nList[duck] = true;
+
+            sb.append(temp).append("\n");
+        }
+
+        System.out.println(sb.toString());
     }
 
     public static void main(String[] args) {
