b3c49f16f05f0b38d5e7c026ceec0caef5c50a23
2giduck <cosmos7256@naver.com>
2022-04-26 22:14:26 +0900

    220426

--- 백준/JSH/Silver/tier1/_1697_숨바꼭질.java
+++ 백준/JSH/Silver/tier1/_1697_숨바꼭질.java
@@ -1,22 +1,54 @@
 package study_of_hell.백준.JSH.Silver.tier1;
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
-public class re_1697_숨바꼭질 {
+public class _1697_숨바꼭질 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
     static void input() {
+        n = scan.nextInt();
+        k = scan.nextInt();
+        dist = new int[100000+1];
     }
 
-    // 숨바꼭질 시작~
-    static void bfs() {
+    static int n;
+    static int k;
+    static int[] dist;
 
+    static void bfs(int x) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(x);
+        dist[x] = 0;
+
+        while(!queue.isEmpty()){
+            int a = queue.poll();
+
+            for (int i = 0; i < 3; i++) {
+                int b;
+                if(i == 0){
+                    b = a - 1;
+                } else if(i == 1){
+                    b = a + 1;
+                } else{
+                    b = a * 2;
+                }
+                if(b < 0 || b > 100000) continue;
+                if(dist[b] != 0) continue;  // 이미 갔던 점이라면
+                dist[b] = dist[a] + 1;
+                queue.add(b);
+            }
+        }
     }
 
     static void pro() {
+        if(n == k){
+            System.out.println(0);
+        }else{
+            bfs(n);
+            System.out.println(dist[k]);
+        }
     }
 
     public static void main(String[] args) {
