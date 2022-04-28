92905dcb0249450e4a952c1c9821a114de0abb6c
2giduck <cosmos7256@naver.com>
2022-04-28 22:13:14 +0900

    220428

--- 백준/JSH/Silver/tier1/_2343_기타_레슨.java
+++ 백준/JSH/Silver/tier1/_2343_기타_레슨.java
@@ -2,24 +2,54 @@ package study_of_hell.백준.JSH.Silver.tier1;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class re_2343_기타_레슨 {
+public class _2343_기타_레슨 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int n;
-    static int m;
-    static int[] adj;
-
     static void input(){
-        n= scan.nextInt();
-        m=scan.nextInt();
-        adj = new int[n+1];
-        for (int i = 1; i <=n; i++) {
-            adj[i] = scan.nextInt();
+        n = scan.nextInt();
+        m = scan.nextInt();
+        nList = new int[n];
+        for (int i = 0; i < n; i++) {
+            nList[i] = scan.nextInt();
         }
     }
 
+    static int n;
+    static int m;
+    static int[] nList;
+
+    static boolean check(int l){
+        int count = 1;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if(sum + nList[i] > l){
+                sum = nList[i];
+                count++;
+            }else{
+                sum += nList[i];
+            }
+        }
+
+        return count <= m;
+    }
+
     static void pro(){
+        int l = 1, r = 1000000000;
+        for (int i = 0; i < n; i++) l = Math.max(l, nList[i]);
+        int ans = 1;
+
+        while(l <= r){
+            int mid = (l + r)/2;
+            if(check(mid)){
+                r = mid - 1;
+                ans = mid;
+            }else{
+                l = mid + 1;
+            }
+        }
+
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
