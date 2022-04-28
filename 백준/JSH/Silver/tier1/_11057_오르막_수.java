b89bfb6bc3e2f52bcf4ced9ac3b8f75f0ab4feb5
2giduck <cosmos7256@naver.com>
2022-04-28 22:34:13 +0900

    220428

--- 백준/JSH/Silver/tier1/_11057_오르막_수.java
+++ 백준/JSH/Silver/tier1/_11057_오르막_수.java
@@ -4,16 +4,44 @@ package study_of_hell.백준.JSH.Silver.tier1;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class re_11057_오르막_수 {
+public class _11057_오르막_수 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-
     static void input(){
+        n = scan.nextInt();
+        dy = new int[n+1][10];
+    }
+
+    static int n;
+    static int[][] dy;
+
+    static void preprocess(){
+        //init
+        for (int i = 0; i <= 9; i++) {
+            dy[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10 ; j++) {
+                for (int k = 0; k <= j; k++) {
+                    dy[i][j] += dy[i-1][k];
+                    dy[i][j] = (dy[i][j])%10007;
+                }
+            }
+        }
     }
 
     static void pro(){
+        preprocess();
 
+        int ans = 0;
+        for (int i = 0; i < 10; i++) {
+            ans += dy[n][i];
+            ans = ans %10007;
+        }
+
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
