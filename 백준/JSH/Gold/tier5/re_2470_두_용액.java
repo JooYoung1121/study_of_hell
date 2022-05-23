a60f2b80e9cfedbb5ce4553d5bd770b6d113bdfa
2giduck <cosmos7256@naver.com>
2022-05-23 21:44:09 +0900

    220523

--- 백준/JSH/Gold/tier5/re_2470_두_용액.java
+++ 백준/JSH/Gold/tier5/re_2470_두_용액.java
@@ -1,22 +1,16 @@
 package study_of_hell.백준.JSH.Gold.tier5;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class re_2470_두_용액 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int n;
-    static int[] adj;
-
     static void input() {
-        n= scan.nextInt();
-        adj = new int[n+1];
-        for (int i = 1; i <=n ; i++) {
-            adj[i] = scan.nextInt();
-        }
     }
+
     static void pro(){
     }
 
